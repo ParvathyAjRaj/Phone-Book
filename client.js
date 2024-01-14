81861fb88dc4712995202debef8897d1958fdda4
ParvathyAjRaj <paruvathy99@gmail.com>
2024-01-14 22:06:50 +0530

    initial commit

---
+++ client.js
@@ -0,0 +1,80 @@
+import express from "express";
+import bodyParser from "body-parser";
+import axios from "axios";
+
+const app=express();
+const port = 3000;
+const API_URL = "http://localhost:2000";
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+
+// Get all posts
+app.get("/phonebook",async(req,res) => {
+ const response = await axios.get(`${API_URL}/phonebook`);
+ res.render("home.ejs",{
+    details : response.data
+ })
+})
+
+// Get a specific post by id
+app.get("/phonebook/:id",async(req,res) => {
+    let id = req.params.id;
+    const response = await axios.get(`${API_URL}/phonebook/${id}`);
+    let details = [response.data];
+    res.render("home.ejs",{
+        details : details
+    })
+})
+
+// on clicking add option in home page, go to addUser.ejs
+app.get("/new",async(req,res) => {
+    res.render("addUser.ejs");
+})
+
+// on clicking add button in addUser.ejs, ask server to add this user
+app.post("/add",async(req,res) => {
+    const response = await axios.post(`${API_URL}/add`,req.body);
+    res.render("home.ejs",{
+        details : response.data
+    })
+})
+
+// on clicking edit option in home page, go to editUser.ejs
+app.get("/edit",(req,res) => {
+    res.render("editUser.ejs");
+})
+
+// on clicking edit button in editUser.ejs, ask server to edit the user
+app.post("/update",async(req,res)=>{
+    let req_details = {id:req.body.id,name:req.body.name,contact:req.body.contact};
+    const response = await axios.patch(`${API_URL}/edit`,req_details);
+    console.log(response.data);
+    res.render("home.ejs",{
+        details : response.data
+    })
+})
+
+// on clicking delete option in home page, go to deleteUser.ejs
+app.get("/del",(req,res) => {
+    res.render("deleteUser.ejs")
+})
+
+// on clicking delete button in deleteUser.ejs, ask server to delete any user
+app.post("/delete",async(req,res) => {
+    console.log(req.body);
+    let id = req.body.id;
+    const response = await axios.delete(`${API_URL}/remove/${id}`);
+    res.render("home.ejs",{details:response.data});
+})
+
+// Listening to the port
+app.listen(port,()=>{
+    console.log(`Client listening to port ${port}`);
+}).on("error", function(err){console.log(`cannot start because of ${err}`)})
+
+// Listening to the port in ip address
+// app.listen(port,"192.168.1.6",()=>{
+//     console.log(`Client listening to port ${port}`);
+// }).on("error", function(err){console.log(`cannot start because of ${err}`)})
---
+++ server.js
@@ -0,0 +1,70 @@
+import express from "express";
+import bodyParser from "body-parser";
+
+const app = express();
+const port = 2000;
+
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.json())
+
+let posts=[
+    {id:1,name:"Ajay",contact:"12345"},
+    {id:2,name:"Parvathy",contact:"34567"},
+    {id:3,name:"Vaani",contact:"56789"}
+]
+
+// give all posts 
+app.get("/phonebook",(req,res)=>{
+    res.json(posts);
+})
+
+// give post with specific id
+app.get("/phonebook/:id",(req,res)=>{
+    const user_id = parseInt(req.params.id);
+    const result = posts.find((post) => post.id === user_id);
+    res.json(result);
+})
+
+// add new post to the list
+app.post("/add",(req,res) => {
+    const new_user = {
+        id : parseInt(req.body.id),
+        name : req.body.name,
+        contact : req.body.contact
+    }
+    posts.push(new_user);
+    res.json(posts);
+})
+
+// edit one of the posts
+app.patch("/edit",(req,res)=> {
+    const post_id = parseInt(req.body.id);
+    let post_index = posts.findIndex((post) => post_id===post.id)
+    const new_name = req.body.name || posts[post_index].name;
+    const new_contact = req.body.contact || posts[post_index].contact;
+    let updated_post = {
+            id : post_id,
+            name : new_name,
+            contact : new_contact
+        };
+    posts[post_index] = updated_post;
+    res.json(posts);
+})
+
+// delete the post
+app.delete("/remove/:id",(req,res) => {
+    let remove_id = parseInt(req.params.id);
+    // splice method => splice(starting index, no of elements to be removed from starting index)
+    let index = posts.findIndex((post) => post.id === remove_id);
+    if(index > -1){
+        posts.splice(index,1);
+        res.json(posts);
+    }
+    else{
+        res.json(posts);
+    }
+})
+
+app.listen(port,()=>{
+    console.log(`Server running on port ${port}`);
+})
